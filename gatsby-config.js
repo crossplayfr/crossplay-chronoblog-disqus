0ee2d62949e222479a8756a2fce1e93ced1ffa41
Yanis Abounacer <yanis.abounacer@outlook.fr>
2021-08-29 15:36:30 +0200

    Author pic and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,8 @@ module.exports = {
     siteLanguage: 'fr',
     ogLanguage: `fr_FR`,
     author: 'CrossPlay', // for example - 'Ivan Ganev'
-    authorDescription: 'short author description', // short text about the author
+    authorDescription:
+      'Notre passion est aussi notre métier, et toute la rédaction CrossPlay se mobilise pour produire du contenu original régulièrement !', // short text about the author
     avatar: '/avatar.jpg',
     twitterSite: '', // website account on twitter
     twitterCreator: '', // creator account on twitter
@@ -73,22 +74,22 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-          // The property ID; the tracking code won't be generated without it
-          trackingId: "UA-83662027-1",
-          // Defines where to place the tracking script - `true` in the head and `false` in the body
-          head: true,
-          // Setting this parameter is optional
-          anonymize: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Delays sending pageview hits on route update (in milliseconds)
-          pageTransitionDelay: 0,
-          // Defers execution of google analytics script after page load
-          defer: false,
-          // defaults to false
-          enableWebVitalsTracking: true,
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-83662027-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // defaults to false
+        enableWebVitalsTracking: true,
       },
     },
     {
